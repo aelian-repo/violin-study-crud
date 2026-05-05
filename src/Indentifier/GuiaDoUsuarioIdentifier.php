28ac061ab831d52ed140c0824b64e1af981fd68e
mateusoliversants <mateusoliversantos2006@gmail.com>
2026-05-05 09:40:46 -0300

    Implementação da pasta Identifier com os arquivos GuiaDoUsuarioIdentifier e UsuarioAssinantes

---
+++ src/Indentifier/GuiaDoUsuarioIdentifier.php
@@ -0,0 +1,135 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         1.0.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace App\Identifier;
+
+use Authentication\Identifier\Resolver\ResolverAwareTrait;
+use Authentication\Identifier\Resolver\ResolverInterface;
+use Authentication\Identifier\AbstractIdentifier;
+use Cake\Datasource\FactoryLocator;
+use App\UsuarioAssinantes;
+
+class GuiaDoUsuarioIdentifier extends AbstractIdentifier
+{
+    use ResolverAwareTrait;
+    /**
+     * @inheritDoc
+     */
+    public function identify(array $data)
+    {
+        $identity = null;
+        $user = $this->getFormUser($data);
+        $usuarioAssinante = $this->login($user);
+        if (is_string($usuarioAssinante)) {
+            $this->_errors[] = $usuarioAssinante;
+        } else {
+            $identity = $this->getUserEntity($data);
+            if (!empty($identity)) {
+                if ($identity === true) {
+                    $identity = $usuarioAssinante;
+                } else {
+                    $identity->usuario_assinante = $usuarioAssinante;
+                }
+            } else {
+                $this->_errors[] = 'Usuário não encontrado.';
+            }
+        }
+
+        return $identity;
+    }
+
+    public function getFormUser(array $data): array
+    {
+        $user = [];
+        $assinanteFields = $this->getConfig('fields.Assinante');
+        $usuarioAssinanteFields = $this->getConfig('fields.UsuarioAssinante');
+        if (is_array($assinanteFields)) {
+            $user['Assinante'] = [];
+            foreach ($assinanteFields as $guiaField => $formField) {
+                if (isset($data[$formField])) {
+                    $user['Assinante'][$guiaField] = $data[$formField];
+                }
+            }
+        }
+        if (is_array($usuarioAssinanteFields)) {
+            $user['UsuarioAssinante'] = [];
+            foreach ($usuarioAssinanteFields as $guiaField => $formField) {
+                if (isset($data[$formField])) {
+                    $user['UsuarioAssinante'][$guiaField] = $data[$formField];
+                }
+            }
+        }
+
+        return $user;
+    }
+
+    public function login(array $user)
+    {
+        $usuarioTable = new UsuarioAssinantes();
+        $usuarioLogado = $usuarioTable->login($user);
+        if ($usuarioLogado === null) {
+            return 'Não foi possível acessar. Tente novamente mais tarde.';
+        }
+
+        if (!isset($usuarioLogado['UsuarioAssinante'])) {
+            if (isset($usuarioLogado['erros'])) {
+                if (isset($usuarioLogado['erros']['login'][0])) {
+                    $usuarioLogado = $usuarioLogado['erros']['login'][0];
+                } else {
+                    $usuarioLogado = $usuarioLogado['erros'];
+                }
+            }
+        }
+
+        return $usuarioLogado;
+    }
+
+    public function getUserEntity(array $data)
+    {
+        $userModel = $this->getResolver()->getConfig('userModel');
+        if (empty($userModel)) {
+            return true;
+        }
+
+        $userTable = FactoryLocator::get('Table')->get($userModel);
+        $usuarioAssinanteFields = $this->getConfig('fields.UsuarioAssinante');
+        $assinanteFields = $this->getConfig('fields.Assinante');
+        $conditions = [];
+        if (is_array($usuarioAssinanteFields)) {
+            foreach ($usuarioAssinanteFields as $userField => $dataField) {
+                if ($dataField !== self::CREDENTIAL_PASSWORD) {
+                    $conditions[$userField] = $data[$dataField];
+                }
+            }
+        }
+        if (is_array($assinanteFields)) {
+            foreach ($assinanteFields as $dataField) {
+                $conditions[$dataField] = $data[$dataField];
+            }
+        }
+        $entity = $userTable->newEmptyEntity();
+        if ($entity->isAccessible('deleted')) {
+            $conditions[] = $userTable->getAlias() . '.deleted IS NULL';
+        }
+        $contain = $this->getResolver()->getConfig('contain');
+        $entity = $userTable->find()
+            ->where($conditions)
+            ->contain($contain)
+            ->first();
+
+        return $entity;
+    }
+}
---
+++ src/Indentifier/UsuarioAssinantes.php
@@ -0,0 +1,59 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Identifier;
+
+class UsuarioAssinantes
+{
+    protected $params;
+    
+    public function login($usuarioAssinante) 
+    {     
+        $this->params = 'login';
+    
+        return $this->request('POST', $usuarioAssinante);    
+    }
+
+    public function request($method, $resource = array()) 
+    {
+        $resource = json_encode($resource);                                                                   
+        $curl = curl_init($this->getUrl());
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);                                              
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $resource);
+        curl_setopt($curl, CURLOPT_VERBOSE, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+        curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);        
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(                                                           
+           'Content-Type: application/json',                                                         
+           'Content-Length: ' . strlen($resource),                                                        
+           'User-Agent: Aelian-GuiaDoUsuario-Plugin',
+        ));                                                                                           
+        $response = curl_exec($curl);
+        if (curl_errno($curl)) {
+            $response = array('erros' => curl_error($curl));            
+        } else {
+            $response = json_decode($response, true);
+        }
+        curl_close($curl);
+
+        return $response;                
+    }       
+    
+    public function getUrl() 
+    {
+        $url = Configure::read('Onboarding.testUrl');
+        if (Configure::read('debug') == 0) {
+            $url = Configure::read('Onboarding.url');            
+        }
+        $path = 'api-' . Inflector::dasherize(Inflector::pluralize($this->getName()));
+        $versao = Configure::read('App.onboardingVersao') . '/';
+        $chave = Configure::read('App.onboardingChave') . '/';      
+        $params = !empty($this->params) ? '/call/' . $this->params : '';
+        $url .= $versao . $chave . $path . $params; 
+
+        return $url;
+    }
+}
